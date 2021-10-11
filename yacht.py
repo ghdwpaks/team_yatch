2fdfcbea8a17585f1c1df494b996fe8492486ea5
USER-PC\user <alstjrdlrk12@naver.com>
2021-10-12 00:19:42 +0900

    temp

--- yacht.py
+++ yacht.py
@@ -1,4 +1,5 @@
 import random
+from typing import Counter
 import copy as c
 import pandas as pd
 
@@ -266,6 +267,7 @@ print("프로그램을 시작합니다.")
 print("1. 게임시작")
 print("2. 게임설명")
 input_start = input("입력 : ")
+count = 0
 
 if input_start == "1" :
     #게임 시작 코드 추가 필요
